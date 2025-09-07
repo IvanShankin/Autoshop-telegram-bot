d82fca4b31a6566f9f8dba730c129fafe23a3450
IvanShankin <ivan_shankin06@mail.ru>
2025-09-07 17:48:34 +0300

    added CI

--- src/config.py
+++ src/config.py
@@ -5,21 +5,19 @@ from dotenv import load_dotenv
 from pathlib import Path
 
 
-
 load_dotenv()
 TOKEN_BOT = os.getenv('TOKEN_BOT')
 MAIN_ADMIN = int(os.getenv('MAIN_ADMIN'))
-
-TYPE_PAYMENTS = {'crypto_bot': 'crypto_bot'}
-TYPE_ACCOUNT_SERVICES = {'telegram': 'telegram', 'other': 'other'}
-
-ALLOWED_LANGS = ["ru", "en"]
-
+SECRET_KEY = os.getenv('SECRET_KEY')
+ALGORITHM = "HS256"
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 sys.path.append(str(BASE_DIR))
-
 LOCALES_DIR = BASE_DIR / 'locales'
+
+TYPE_PAYMENTS = {'crypto_bot': 'crypto_bot'}
+TYPE_ACCOUNT_SERVICES = {'telegram': 'telegram', 'other': 'other'}
+ALLOWED_LANGS = ["ru", "en"]
 DEFAULT_LANG = "ru"
 
 DT_FORMAT_FOR_LOGS = "%Y-%m-%d %H:%M:%S"

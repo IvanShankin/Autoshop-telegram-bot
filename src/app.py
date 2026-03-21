12aec198ebf0340e4e68192d0cb84e0310f51367
IvanShankin <ivan_shankin06@mail.ru>
2026-03-21 22:18:55 +0300

    added start_app

---
+++ src/app.py
@@ -0,0 +1,50 @@
+import asyncio
+
+from src.broker.consumer import start_background_consumer, stop_background_consumer
+from src.deferred_tasks.core import init_scheduler
+from src.services.database.backups.backup_db import add_backup_create, add_backup_cleanup
+from src.services.database.core.filling_database import create_database
+from src.services.fastapi_core.server import start_server
+from src.services.redis.core_redis import init_redis, close_redis
+from src.services.redis.filling import filling_all_redis
+from src.services.database.discounts.utils.set_not_valid import deactivate_expired_promo_codes_and_vouchers
+from src.bot_actions.bot_run import run_bot
+from src.services.redis.tasks import start_dollar_rate_scheduler
+from src.services.secrets import init_crypto_context
+from src.config import get_config, init_config
+from src.utils.core_logger import setup_logging
+
+
+async def start_app():
+    """
+    Асинхронный контекстный менеджер для запуска приложения.
+    Инициализирует конфиг, Redis, Database, запуск отложенных задач.
+    """
+    init_config()  # конфиги необходимо до init_crypto_context
+    await init_redis()
+
+    try:
+        init_crypto_context()
+    except RuntimeError as e: # если уже есть
+        pass
+
+    setup_logging(get_config().paths.log_file)
+    await create_database()
+    await filling_all_redis()
+    await start_background_consumer()
+
+    asyncio.create_task(deactivate_expired_promo_codes_and_vouchers())
+    asyncio.create_task(start_dollar_rate_scheduler())
+    asyncio.create_task(start_server())
+
+    # отложенный задачник
+    scheduler = init_scheduler()
+    add_backup_create(scheduler)
+    add_backup_cleanup(scheduler)
+    scheduler.start()
+
+    try:
+        await run_bot()
+    finally:
+        await stop_background_consumer()
+        await close_redis()
--- src/main.py
+++ src/main.py
@@ -1,60 +1,19 @@
 import asyncio
 
-from src.broker.consumer import start_background_consumer, stop_background_consumer
-from src.deferred_tasks.core import init_scheduler
-from src.services.database.backups.backup_db import add_backup_create, add_backup_cleanup
-from src.services.database.core.filling_database import create_database
-from src.services.fastapi_core.server import start_server
-from src.services.redis.core_redis import init_redis, close_redis
-from src.services.redis.filling import filling_all_redis
-from src.services.database.discounts.utils.set_not_valid import deactivate_expired_promo_codes_and_vouchers
-from src.bot_actions.bot_run import run_bot
-from src.services.redis.tasks import start_dollar_rate_scheduler
-from src.services.secrets import init_crypto_context
-from src.config import get_config, init_config
-from src.utils.core_logger import setup_logging, get_logger
+from src.app import start_app
+from src.utils.core_logger import get_logger
 
 
-async def on_startup():
-    init_config()  # конфиги необходимо до init_crypto_context
-    await init_redis()
-
-    try:
-        init_crypto_context()
-    except RuntimeError as e: # если уже есть
-        pass
-
-    setup_logging(get_config().paths.log_file)
-    await create_database()
-    await filling_all_redis()
-    await start_background_consumer()
-
-    asyncio.create_task(deactivate_expired_promo_codes_and_vouchers())
-    asyncio.create_task(start_dollar_rate_scheduler())
-    asyncio.create_task(start_server())
-
-    # отложенный задачник
-    scheduler = init_scheduler()
-    add_backup_create(scheduler)
-    add_backup_cleanup(scheduler)
-    scheduler.start()
-
-    await run_bot()
-
-
-async def on_shutdown():
-    await stop_background_consumer()
-    await close_redis()
+async def main():
+    await start_app()
 
 
 if __name__ == '__main__':
     logger = get_logger(__name__)
     try:
         logger.info("Бот начал работу")
-        asyncio.run(on_startup())
+        asyncio.run(main())
     except KeyboardInterrupt:
         logger.info("Бот завершил работу")
     except Exception as e:
         logger.exception(f"ошибка: {e}")
-    finally:
-        asyncio.run(on_shutdown())
